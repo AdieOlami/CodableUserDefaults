6483927bae15cd00600e798b98fc436a464ca32e
Adie Olalekan <ugbeadie@gmail.com>
2019-09-11 18:10:45 +0100

    Added key parameters

---
+++ CodableUserDefaultsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodableUserDefaultsExample
+//
+//  Created by Olar's Mac on 8/13/19.
+//  Copyright © 2019 Adie Olalekan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

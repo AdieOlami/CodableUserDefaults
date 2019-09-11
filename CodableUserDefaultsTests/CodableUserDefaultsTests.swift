6be8b50d2b5e0dd916e52d5785545b12e84dca7c
Adie Olalekan <ugbeadie@gmail.com>
2019-09-11 18:42:42 +0100

    updated CodableUserDefaults.podspec

--- CodableUserDefaultsTests/CodableUserDefaultsTests.swift
+++ CodableUserDefaultsTests/CodableUserDefaultsTests.swift
@@ -10,18 +10,18 @@ import XCTest
 @testable import CodableUserDefaults
 
 class CodableUserDefaultsTests: XCTestCase {
+    
+    var cUD: CodableUserDefaults!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        cUD = CodableUserDefaults()
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSave() {
+        
     }
 
     func testPerformanceExample() {
